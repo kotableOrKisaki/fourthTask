dad41ae5f699e36292625b1a8621cfc9946f0b8a
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:12:49 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fourthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int totalAlbumImages = 52;
+            int filledRowsCount;
+            int overageImages;
+
+            filledRowsCount = totalAlbumImages / imagesInRow;
+            overageImages = totalAlbumImages % imagesInRow;
+
+            Console.WriteLine("Полностью заполненных картинками рядов: {0}", filledRowsCount);
+            Console.WriteLine("Картинок сверх меры: {0}", overageImages);
+        }
+    }
+}
